f0a70d597b2fe0cdc81521250868f75fd66599fa
Anton Stoyanov <77524568+aStoyanov91@users.noreply.github.com>
2023-04-05 03:03:55 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/aStoyanov91/tetris",
+  assetPrefix: "/aStoyanov91/tetris",
+};
 
 module.exports = nextConfig
